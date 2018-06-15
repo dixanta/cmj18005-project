748076d6d8640bf6203e3fa70cee90d939162d5e
dixanta <dixanta@gmail.com>
2018-06-15 09:07:13 +0545

    Profile Added

--- src/main/java/com/herokuapp/constant/DbConstant.java
+++ src/main/java/com/herokuapp/constant/DbConstant.java
@@ -16,7 +16,7 @@ public class DbConstant {
     public static final String DB_Password="admin";*/
     
     public static final String DB_DRIVER="org.postgresql.Driver";
-    public static final String DB_URL="jdbc:postgresql ://ec2-54-243-235-153.compute-1.amazonaws.com/dfuvi9rnjh6ft0?sslmodel=require";
+    public static final String DB_URL="jdbc:postgresql://ec2-54-243-235-153.compute-1.amazonaws.com/dfuvi9rnjh6ft0?sslmodel=require";
     public static final String DB_USER="hwpvxglkdiajsq";
     public static final String DB_Password="7f52364846b62b169c5ce3ffc2cc550dfb70e67df23078b821d4e4b07e5b7a20";
 }
